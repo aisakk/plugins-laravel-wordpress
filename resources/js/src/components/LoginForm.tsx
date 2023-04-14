b4b74a5cc76feb382b04c80eb85a9ea4360874bd
Aisakk <santaellaisaac2@gmail.com>
2023-04-14 18:32:59 -0400

    Configurando el diseño del login para que se vea centrado en la pantalla

--- resources/js/src/components/LoginForm.tsx
+++ resources/js/src/components/LoginForm.tsx
@@ -22,7 +22,7 @@ const LoginForm = (props:LoginInterface) => {
     };
 
     return (
-            <div className="max-w-md w-full space-y-8">
+            <div className="xl:max-w-xl max-w-md  w-full space-y-8">
                 <div>
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                         Iniciar sesión
